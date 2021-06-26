3d924175fec9cdba1bf12d815bbc535d0d169be0
Alexios Karadimos <karadalex@gmail.com>
2021-06-26 19:30:51 +0300

    move data to separate data file

---
+++ data/posts.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    title: "First blog post",
+    description: "this is a description",
+    tags: ["tag1", "tag2", "tag3"],
+    image: "https://images.unsplash.com/photo-1511818966892-d7d671e672a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
+    slug: "first-blog-post",
+    date: "23/06/2021"
+  },
+  {
+    title: "Second blog post",
+    description: "this is a description",
+    tags: ["tag1", "tag2", "tag3"],
+    image: "https://images.unsplash.com/photo-1511818966892-d7d671e672a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
+    slug: "second-blog-post",
+    date: "23/06/2021"
+  },
+  {
+    title: "Third blog post",
+    description: "this is a description",
+    tags: ["tag1", "tag2", "tag3"],
+    image: "https://images.unsplash.com/photo-1511818966892-d7d671e672a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
+    slug: "third-blog-post",
+    date: "23/06/2021"
+  },
+  {
+    title: "Fourth blog post",
+    description: "this is a description",
+    tags: ["tag1", "tag2", "tag3"],
+    image: "https://images.unsplash.com/photo-1511818966892-d7d671e672a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
+    slug: "fourth-blog-post",
+    date: "23/06/2021"
+  }
+]
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
-const posts = [
-  {
-    title: "First blog post",
-    description: "this is a description",
-    tags: ["tag1", "tag2", "tag3"],
-    image: "https://images.unsplash.com/photo-1511818966892-d7d671e672a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
-    slug: "first-blog-post",
-    date: "23/06/2021"
-  },
-  {
-    title: "Second blog post",
-    description: "this is a description",
-    tags: ["tag1", "tag2", "tag3"],
-    image: "https://images.unsplash.com/photo-1511818966892-d7d671e672a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
-    slug: "second-blog-post",
-    date: "23/06/2021"
-  },
-  {
-    title: "Third blog post",
-    description: "this is a description",
-    tags: ["tag1", "tag2", "tag3"],
-    image: "https://images.unsplash.com/photo-1511818966892-d7d671e672a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
-    slug: "third-blog-post",
-    date: "23/06/2021"
-  },
-  {
-    title: "Fourth blog post",
-    description: "this is a description",
-    tags: ["tag1", "tag2", "tag3"],
-    image: "https://images.unsplash.com/photo-1511818966892-d7d671e672a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
-    slug: "fourth-blog-post",
-    date: "23/06/2021"
-  }
-]
+import posts from "../data/posts"
 
 export default function Home() {
   return (
